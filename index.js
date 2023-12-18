a0609bc0a5407480e0952c2e81fbda292589338a
Victor <victor.rasmussen.vr@gmail.com>
2023-12-18 14:47:56 +0100

    Add send To server function

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ function sendMessage(event) {
 
     // Append the user's message to the chat
     appendMessage("User", userInput);
+
+    // Send the message to the server
+    sendToServer(userInput);
   }
+}
+
+function sendToServer(message) {
+  fetch("http://localhost:1234", {
+    // Replace with your server's address and port
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ message: message }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Process the response from the server (if needed)
+      console.log("Server response:", data);
+    })
+    .catch((error) => {
+      console.error("Error sending message to server:", error);
+    });
 }
 
 function appendMessage(sender, message) {
